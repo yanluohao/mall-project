20eee598731b56c8f43eaf7fa6a4a08ed0f44b1f
ylh <345072986@qq.com>
2017-12-12 18:25:43 +0800

    node获取mongoose数据，提供前端api接口

--- src/main.js
+++ src/main.js
@@ -3,13 +3,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import VueLazyload from 'vue-lazyload'
+import infiniteScroll from 'vue-infinite-scroll'
+import {currency} from './util/currency'
+
+Vue.use(infiniteScroll);
+Vue.use(Vuex);
+Vue.use(VueLazyload, {
+    loading: 'static/loading-svg/loading-bars.svg',
+    try: 3 // default 1
+})
+
+Vue.filter("currency", currency);
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: {App}
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList'
+import Cart from '@/views/Cart'
+import Address from '@/views/Address'
+import OrderConfirm from '@/views/OrderConfirm'
+import OrderSuccess from '@/views/OrderSuccess'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      {
+          path: '/',
+          name:'GoodsList',
+          component: GoodsList
+      },
+      {
+          path: '/Cart',
+          name:'Cart',
+          component: Cart
+      },
+      {
+          path: '/Address',
+          name:'Address',
+          component: Address
+      },
+      {
+          path: '/OrderConfirm',
+          name:'OrderConfirm',
+          component: OrderConfirm
+      },
+      {
+          path: '/OrderSuccess',
+          name:'OrderSuccess',
+          component: OrderSuccess
+      }
+
   ]
 })
